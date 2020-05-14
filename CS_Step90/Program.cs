3a60a471ed33fc3f7bfbe1157def4fc2a2fe4362
Gavin Kimberlin <guviner@gmail.com>
2020-05-14 12:47:59 -0700

    Create a program that will determine who has a higher salary between 2 people.

--- CS_Step90/Program.cs
+++ CS_Step90/Program.cs
@@ -10,6 +10,27 @@ namespace CS_Step90
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anonymous Income Comparison Program\r\nPerson 1\r\nHourly Rate?");
+            byte person1Rate = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week?");
+            byte person1Hours = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Person 2\r\nHourly Rate?");
+            byte person2Rate = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Hours worked per week?");
+            byte person2Hours = Convert.ToByte(Console.ReadLine());
+
+            int person1Salary = person1Rate * person1Hours * 52;
+            int person2Salary = person2Rate * person2Hours * 52;
+            Console.WriteLine("Annual salary of Person 1:\r\n" + person1Salary);
+            Console.WriteLine("Annual salary of Person 2:\r\n" + person2Salary);
+
+            bool makesMore = person1Salary > person2Salary;
+            Console.WriteLine("Does Person 1 make more money than Person 2?\r\n" + makesMore);
+            Console.ReadLine();
+
         }
     }
 }
